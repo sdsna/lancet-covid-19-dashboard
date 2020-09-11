5c9af403cc4dc299ad1c835b33c9d7cdc9a3a1e2
Finn Woelm <finn.woelm@unsdsn.org>
2020-09-11 11:41:04 +0200

    Set up materialUI theme
    Define a better set of font sizes for materialUI.

---
+++ helpers/getTheme.js
@@ -0,0 +1,94 @@
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#1a305b',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    }
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 960,
+      lg: 1140,
+      xl: 1920,
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: '3.7rem',
+      fontWeight: 500
+    },
+    h2: {
+      fontSize: '1.9rem',
+      fontWeight: 400
+    },
+    h3: {
+      fontSize: '1.25rem',
+      fontWeight: 400
+    },
+    h4: {
+      fontSize: '1.15rem',
+      fontWeight: 400
+    },
+    body1: {
+      lineHeight: 1.25,
+    },
+    subtitle1: {
+      fontSize: '1.25rem',
+      fontWeight: 300,
+      lineHeight: 1.33
+    },
+    subtitle2: {
+      fontSize: '1rem'
+    },
+    caption: {
+      fontSize: '.7rem',
+      letterSpacing: 0,
+      lineHeight: 1.2,
+    },
+    overline: {
+      fontSize: '.7rem',
+      letterSpacing: 0,
+      textTransform: 'uppercase',
+      lineHeight: 1,
+      marginBottom: 8,
+    }
+  },
+  props: {
+    MuiTypography: {
+      variantMapping: {
+        h1: 'h1',
+        h2: 'h2',
+        h3: 'h3',
+        h4: 'h4',
+        subtitle1: 'p',
+        subtitle2: 'p',
+        caption: 'p',
+        overline: 'p',
+      },
+    },
+  },
+  overrides: {
+    MuiTableCell: {
+      body: {
+        fontSize: '1rem'
+      },
+      head: {
+        fontSize: '1.1rem'
+      }
+    }
+  }
+});
+
+const themeWithResponsiveFontSizes = responsiveFontSizes(theme)
+
+const getTheme = () => {
+  return themeWithResponsiveFontSizes
+}
+
+export default getTheme
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import App from 'next/app'
 import Head from 'next/head'
 import { useEffect } from 'react'
-import { CssBaseline } from '@material-ui/core'
+import { CssBaseline, ThemeProvider } from '@material-ui/core'
+import getTheme from '../helpers/getTheme'
 
 function MyApp({ Component, pageProps }) {
   // Remove the server-side injected CSS.
@@ -18,9 +19,11 @@ function MyApp({ Component, pageProps }) {
         <title>Lancet Covid-19 Dashboard</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <Component {...pageProps} />
+      <ThemeProvider theme={getTheme()}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   )
 }
