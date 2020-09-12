89522b5b5eead78dc50bcef344382c3444c2582a
Finn Woelm <finn.woelm@unsdsn.org>
2020-09-12 17:44:35 +0200

    Map: Color countries according to their value
    Use d3-scale to create a white-blue color scale for shading countries 
    according to their value.

--- pages/map.js
+++ pages/map.js
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
+import { scaleLinear } from "d3-scale";
 import styled from "styled-components";
 
 import MapLayout from "layouts/MapLayout";
@@ -7,7 +8,7 @@ import MapTooltip from "components/MapTooltip";
 
 import getIndicatorProps from "helpers/getIndicatorProps";
 
-const Map = ({ countries, observations }) => {
+const Map = ({ countries, observations, indicator }) => {
   const [mapData, setMapData] = useState([]);
 
   const getTooltipLabel = useCallback((countryId) => countries[countryId], [
@@ -21,16 +22,25 @@ const Map = ({ countries, observations }) => {
   );
 
   useEffect(() => {
+    const { max, min } = indicator;
+
+    const colorScale = scaleLinear()
+      .domain([min, max])
+      .range(["#bbe4fd", "#0084d5"])
+      .clamp(true);
+
     const data = Object.keys(observations).map((id) => {
+      const value = observations[id]["2020-09-10"];
+
       return {
         countryId: id,
-        fill: "blue",
-        value: observations[id]["2020-09-10"],
+        fill: colorScale(value),
+        value,
       };
     });
 
     setMapData(data);
-  }, [observations]);
+  }, [observations, indicator]);
 
   return (
     <MapLayout mobileMenuLabel="Settings">
@@ -47,10 +57,25 @@ const Map = ({ countries, observations }) => {
 export async function getStaticProps() {
   const { countries, observations } = await getIndicatorProps("jhu_confirmed");
 
+  // Evaluate max and min values for indicator
+  // TODO: Calculate better bounds for color scale (perhaps based on 2.5th
+  //       percentile)
+  const values = [
+    ...Object.values(observations).flatMap((dataPoints) => [
+      ...Object.values(dataPoints),
+    ]),
+  ];
+  const max = Math.max(...values);
+  const min = Math.min(...values);
+
   return {
     props: {
       countries,
       observations,
+      indicator: {
+        max,
+        min,
+      },
     },
   };
 }
