b05f951976081a8765fc4befc77d4b9c9b23c2e1
Finn Woelm <finn.woelm@unsdsn.org>
2020-10-02 18:08:15 +0200

    Display last extraction time as "X hours ago"
    This makes it easier to see how up-to-date the data is.

--- components/MapLastUpdated.js
+++ components/MapLastUpdated.js
@@ -1,16 +1,37 @@
-import { Box, Typography } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import { Box, Tooltip, Typography } from "@material-ui/core";
 import { Update } from "mdi-material-ui";
+import { formatDistanceToNowStrict, parse } from "date-fns";
 
-const MapLastUpdated = ({ updatedAt }) => (
-  <Box
-    position="absolute"
-    display="flex"
-    padding={0.5}
-    style={{ top: 0, right: 0, background: "rgba(255, 255, 255, 0.7)" }}
-  >
-    <Update fontSize="small" style={{ marginRight: 4 }} />
-    <Typography variant="body1">Updated: {updatedAt}</Typography>
-  </Box>
-);
+const MapLastUpdated = ({ updatedAt }) => {
+  const [text, setText] = useState(updatedAt);
+
+  useEffect(() => {
+    const timeAgo = formatDistanceToNowStrict(
+      parse(`${updatedAt}+00`, "MMM dd, yyyy HH:mm 'GMT'x", new Date())
+    );
+    setText(`${timeAgo} ago`);
+  }, [updatedAt]);
+
+  return (
+    <Box
+      position="absolute"
+      style={{
+        top: 0,
+        right: 0,
+        background: "rgba(255, 255, 255, 0.7)",
+        cursor: "help",
+        userSelect: "none",
+      }}
+    >
+      <Tooltip title={`Data was extracted on ${updatedAt}`}>
+        <Box display="flex" padding={0.5}>
+          <Update fontSize="small" style={{ marginRight: 4 }} />
+          <Typography variant="body1">Updated: {text}</Typography>
+        </Box>
+      </Tooltip>
+    </Box>
+  );
+};
 
 export default MapLastUpdated;
