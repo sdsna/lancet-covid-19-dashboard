86d7e5fc29be1dc21bf035a701ffdc0387db2178
Finn Woelm <finn.woelm@unsdsn.org>
2020-10-07 14:25:47 +0200

    NavBar dropdown menu: Close on click
    Close the dropdown menu when the user makes a selection.

--- components/NavBarDropdownMenu.js
+++ components/NavBarDropdownMenu.js
@@ -2,19 +2,27 @@ import Link from "next/link";
 import { MenuList, MenuItem } from "@material-ui/core";
 import DropdownMenu from "components/DropdownMenu";
 
-const NavBarDropdownMenu = ({ pages, ...otherProps }) => {
+const NavBarDropdownMenu = ({ pages, handeClose, ...otherProps }) => {
   if (pages == null || pages.length === 0) return null;
 
   return (
     <DropdownMenu
       placement="bottom-start"
       paperProps={{ elevation: 5, square: true }}
+      handeClose={handeClose}
       {...otherProps}
     >
       <MenuList>
         {pages.map(({ href, label, disabled }) => (
           <Link key={href} href={href} passHref>
-            <MenuItem disabled={disabled} component="a">
+            <MenuItem
+              disabled={disabled}
+              component="a"
+              onClick={() => {
+                if (disabled) return;
+                handleClose();
+              }}
+            >
               {label}
             </MenuItem>
           </Link>
